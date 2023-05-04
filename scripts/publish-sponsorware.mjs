4daa32af76efe5257757f0584b6355c607edccdc
Robin Genz <mail@robingenz.dev>
2023-05-04 19:03:50 +0200

    chore(scripts): set correct `dirName`

--- scripts/publish-sponsorware.mjs
+++ scripts/publish-sponsorware.mjs
@@ -1,29 +1,27 @@
-import * as fs from "fs";
-import { execute } from "./lib/cli.mjs";
-import { exec } from "./lib/subprocess.mjs";
+import * as fs from 'fs';
+import { execute } from './lib/cli.mjs';
+import { exec } from './lib/subprocess.mjs';
 
 execute(async () => {
-  const packagesParam = process.argv.find((arg) =>
-    arg.startsWith("--packages="),
-  );
+  const packagesParam = process.argv.find(arg => arg.startsWith('--packages='));
   if (!packagesParam) {
-    console.error("ERR: --packages parameter is missing.");
+    console.error('ERR: --packages parameter is missing.');
     process.exit(1);
   }
 
-  const packagesParamValue = packagesParam.split("=")[1];
+  const packagesParamValue = packagesParam.split('=')[1];
   const packages = JSON.parse(packagesParamValue);
-  const packagesToPublish = packages.filter((pkg) => {
-    const dirName = pkg.name.split("/")[1];
+  const packagesToPublish = packages.filter(pkg => {
+    const dirName = pkg.name.split('/')[1].replace('capacitor-', '');
     return (
-      pkg.name.startsWith("@capawesome-team/") &&
-      !pkg.version.includes("-") &&
+      pkg.name.startsWith('@capawesome-team/') &&
+      !pkg.version.includes('-') &&
       fs.existsSync(`./packages/${dirName}/package.json`)
     );
   });
 
   for (const pkg of packagesToPublish) {
-    const dirName = pkg.name.split("/")[1];
+    const dirName = pkg.name.split('/')[1].replace('capacitor-', '');
     console.log(`Publishing ${pkg.name}@${pkg.version}...`);
     await exec(
       `npm pkg set repository.url=git+https://github.com/capawesome-team/sponsorware.git`,
