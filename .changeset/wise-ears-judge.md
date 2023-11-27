e3cfdb64de06b974736d9ee1ad320982fa93c587
walweh <134487623+walweh@users.noreply.github.com>
2023-11-27 17:49:26 +0100

    wip

---
+++ .changeset/wise-ears-judge.md
@@ -0,0 +1,5 @@
+---
+'@capawesome-team/capacitor-file-opener': major
+---
+
+iOS file opening function fixed to throw an exception on failure for improved error handling.
--- packages/file-opener/ios/Plugin/FileOpenerPlugin.swift
+++ packages/file-opener/ios/Plugin/FileOpenerPlugin.swift
@@ -9,7 +9,7 @@ import Capacitor
 public class FileOpenerPlugin: CAPPlugin {
     public let errorPathMissing = "path must be provided."
     public let errorFileNotExist = "File does not exist."
-    public let errorCouldNotOpen = "Could not open File."
+    public let errorCannotOpenFile = "File cannot be opened."
 
     private var implementation: FileOpener?
 
@@ -30,7 +30,7 @@ public class FileOpenerPlugin: CAPPlugin {
 
         implementation?.openFile(url: url, mimeType: mimeType, completion: {completionResult in
             if completionResult == false{
-                call.reject(self.errorCouldNotOpen)
+                call.reject(self.errorCannotOpenFile)
             }else{
                 call.resolve()
             }
