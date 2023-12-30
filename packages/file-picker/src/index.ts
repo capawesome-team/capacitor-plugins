fbc4f0a1aabb2a6f199ef3ee1da3a8a995b8ef93
Stephan Fischer <stephan@mrfischer.de>
2023-12-30 09:25:34 +0100

    cleanup

--- packages/file-picker/src/index.ts
+++ packages/file-picker/src/index.ts
@@ -2,7 +2,6 @@ import { registerPlugin } from '@capacitor/core';
 
 import type { FilePickerPlugin } from './definitions';
 // See https://github.com/capawesome-team/capacitor-plugins/issues/14
-// See https://github.com/capawesome-team/capacitor-plugins/issues/14
 import * as web from './web';
 
 const FilePicker = registerPlugin<FilePickerPlugin>('FilePicker', {
