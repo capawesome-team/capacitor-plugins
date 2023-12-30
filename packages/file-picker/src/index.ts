0de0ce2c9a9f4b743870cee5c15675674ee3a812
Stephan Fischer <stephan@mrfischer.de>
2023-12-30 08:28:44 +0100

    npm run fmt

--- packages/file-picker/src/index.ts
+++ packages/file-picker/src/index.ts
@@ -4,7 +4,7 @@ import type { FilePickerPlugin } from './definitions';
 import * as web from './web';
 
 const FilePicker = registerPlugin<FilePickerPlugin>('FilePicker', {
-    web:  () => new web.FilePickerWeb()
+  web: () => new web.FilePickerWeb(),
 });
 
 export * from './definitions';
