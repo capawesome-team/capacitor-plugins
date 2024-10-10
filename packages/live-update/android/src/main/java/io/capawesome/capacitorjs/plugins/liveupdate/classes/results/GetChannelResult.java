f3f725aa8d5c3d16058097ceaadd73b08f105b8b
Robin Genz <mail@robingenz.dev>
2024-10-10 16:44:22 +0200

    fix(android): add missing import

--- packages/live-update/android/src/main/java/io/capawesome/capacitorjs/plugins/liveupdate/classes/results/GetChannelResult.java
+++ packages/live-update/android/src/main/java/io/capawesome/capacitorjs/plugins/liveupdate/classes/results/GetChannelResult.java
@@ -3,6 +3,7 @@ package io.capawesome.capacitorjs.plugins.liveupdate.classes.results;
 import androidx.annotation.Nullable;
 import com.getcapacitor.JSObject;
 import io.capawesome.capacitorjs.plugins.liveupdate.interfaces.Result;
+import org.json.JSONObject;
 
 public class GetChannelResult implements Result {
 
