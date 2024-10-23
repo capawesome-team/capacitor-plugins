b08ff42eb5969a1529704ae3360aa225d5382817
Robin Genz <mail@robingenz.dev>
2024-10-23 16:48:28 +0200

    wip

--- packages/live-update/ios/Plugin/LiveUpdatePlugin.swift
+++ packages/live-update/ios/Plugin/LiveUpdatePlugin.swift
@@ -8,7 +8,7 @@ import Capacitor
 @objc(LiveUpdatePlugin)
 public class LiveUpdatePlugin: CAPPlugin {
     public static let tag = "LiveUpdate"
-    public static let version = "6.5.0"
+    public static let version = "6.6.0"
     public static let userDefaultsPrefix = "CapawesomeLiveUpdate" // DO NOT CHANGE
 
     private var config: LiveUpdateConfig?
