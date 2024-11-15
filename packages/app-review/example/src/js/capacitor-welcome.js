5b51732b861a598990363957d8aa54c0ec48afa7
Robin Genz <mail@robingenz.dev>
2024-11-15 10:10:34 +0100

    style: format

--- packages/app-review/example/src/js/capacitor-welcome.js
+++ packages/app-review/example/src/js/capacitor-welcome.js
@@ -92,24 +92,26 @@ window.customElements.define(
     connectedCallback() {
       const self = this;
 
-      self.shadowRoot.querySelector('#take-photo').addEventListener('click', async function (e) {
-        try {
-          const photo = await Camera.getPhoto({
-            resultType: 'uri',
-          });
+      self.shadowRoot
+        .querySelector('#take-photo')
+        .addEventListener('click', async function (e) {
+          try {
+            const photo = await Camera.getPhoto({
+              resultType: 'uri',
+            });
 
-          const image = self.shadowRoot.querySelector('#image');
-          if (!image) {
-            return;
+            const image = self.shadowRoot.querySelector('#image');
+            if (!image) {
+              return;
+            }
+
+            image.src = photo.webPath;
+          } catch (e) {
+            console.warn('User cancelled', e);
           }
-
-          image.src = photo.webPath;
-        } catch (e) {
-          console.warn('User cancelled', e);
-        }
-      });
+        });
     }
-  }
+  },
 );
 
 window.customElements.define(
@@ -138,5 +140,5 @@ window.customElements.define(
     <slot></slot>
     `;
     }
-  }
+  },
 );
