dccf3b8caf8f60be7ea2d0db3d3020916572c2f9
mertyldrr <mertyildirir14@gmail.com>
2024-11-17 12:42:39 +0100

    feat(app-review): android implementation

--- packages/app-review/android/src/main/java/io/capawesome/capacitorjs/plugins/appreview/AppReview.java
+++ packages/app-review/android/src/main/java/io/capawesome/capacitorjs/plugins/appreview/AppReview.java
@@ -1,11 +1,55 @@
 package io.capawesome.capacitorjs.plugins.appreview;
 
+import android.app.Activity;
 import android.util.Log;
+import com.getcapacitor.Logger;
+import com.google.android.gms.tasks.Task;
+import com.google.android.play.core.review.ReviewInfo;
+import com.google.android.play.core.review.ReviewManager;
+import com.google.android.play.core.review.ReviewManagerFactory;
 
 public class AppReview {
 
-    public String echo(String value) {
-        Log.i("Echo", value);
-        return value;
+    private final ReviewManager manager;
+    private final Activity activity;
+
+    public AppReview(Activity activity) {
+        this.activity = activity;
+        this.manager = ReviewManagerFactory.create(activity);
+    }
+
+    public void requestReviewFlow(ReviewResultCallback<ReviewInfo> callback) {
+        manager
+            .requestReviewFlow()
+            .addOnCompleteListener(
+                task -> {
+                    if (task.isSuccessful()) {
+                        ReviewInfo reviewInfo = task.getResult();
+                        Logger.info("AppReview", "ReviewInfo retrieved successfully.");
+                        callback.success(reviewInfo);
+                    } else {
+                        String error = "Failed to retrieve ReviewInfo.";
+                        Logger.error(error, task.getException());
+                        callback.error(task.getException());
+                    }
+                }
+            );
+    }
+
+    public void launchReviewFlow(ReviewInfo reviewInfo, ReviewResultCallback<Void> callback) {
+        Task<Void> flow = manager.launchReviewFlow(activity, reviewInfo);
+        flow.addOnSuccessListener(
+            result -> {
+                Logger.info("AppReview", "Review flow launched successfully.");
+                callback.success(null);
+            }
+        );
+        flow.addOnFailureListener(
+            exception -> {
+                String error = "Failed to launch review flow.";
+                Logger.error(error, exception);
+                callback.error(exception);
+            }
+        );
     }
 }
--- packages/app-review/android/src/main/java/io/capawesome/capacitorjs/plugins/appreview/AppReviewPlugin.java
+++ packages/app-review/android/src/main/java/io/capawesome/capacitorjs/plugins/appreview/AppReviewPlugin.java
@@ -1,22 +1,67 @@
 package io.capawesome.capacitorjs.plugins.appreview;
 
 import com.getcapacitor.JSObject;
+import com.getcapacitor.Logger;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
+import com.google.android.play.core.review.ReviewInfo;
 
 @CapacitorPlugin(name = "AppReview")
 public class AppReviewPlugin extends Plugin {
 
-    private AppReview implementation = new AppReview();
+    private AppReview appReview;
+    private ReviewInfo reviewInfo;
+
+    @Override
+    public void load() {
+        appReview = new AppReview(getActivity());
+    }
 
     @PluginMethod
-    public void echo(PluginCall call) {
-        String value = call.getString("value");
+    public void openAppStore(PluginCall call) {
+        if (reviewInfo != null) {
+            appReview.launchReviewFlow(
+                reviewInfo,
+                new ReviewResultCallback<Void>() {
+                    @Override
+                    public void success(Void result) {
+                        call.resolve();
+                    }
 
-        JSObject ret = new JSObject();
-        ret.put("value", implementation.echo(value));
-        call.resolve(ret);
+                    @Override
+                    public void error(Exception exception) {
+                        String message = exception.getMessage();
+                        Logger.error(message, exception);
+                        call.reject(message);
+                    }
+                }
+            );
+        } else {
+            call.reject("ReviewInfo is not available. Please call requestReview first.");
+        }
+    }
+
+    @PluginMethod
+    public void requestReview(PluginCall call) {
+        appReview.requestReviewFlow(
+            new ReviewResultCallback<ReviewInfo>() {
+                @Override
+                public void success(ReviewInfo info) {
+                    reviewInfo = info;
+                    JSObject result = new JSObject();
+                    result.put("canLaunchReviewFlow", true);
+                    call.resolve(result);
+                }
+
+                @Override
+                public void error(Exception exception) {
+                    String message = exception.getMessage();
+                    Logger.error(message, exception);
+                    call.reject(message);
+                }
+            }
+        );
     }
 }
