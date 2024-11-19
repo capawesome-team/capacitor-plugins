c6892f04de8b1a3df5383ebc914a0f88068f9549
Robin Genz <mail@robingenz.dev>
2024-11-19 07:49:17 +0100

    wip

--- packages/app-review/ios/Plugin/AppReviewPlugin.m
+++ packages/app-review/ios/Plugin/AppReviewPlugin.m
@@ -4,7 +4,6 @@
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(AppReviewPlugin, "AppReview",
-           CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
            CAP_PLUGIN_METHOD(openAppStore, CAPPluginReturnPromise);
            CAP_PLUGIN_METHOD(requestReview, CAPPluginReturnPromise);
 )
