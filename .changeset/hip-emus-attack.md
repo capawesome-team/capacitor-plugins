bfb1ab8adce5b7735483a9a067803ad8ece3d8f6
Robin Genz <mail@robingenz.dev>
2024-11-24 16:26:38 +0100

    feat(torch): add support for ML Kit Barcode Scanning

---
+++ .changeset/hip-emus-attack.md
@@ -0,0 +1,5 @@
+---
+'@capawesome/capacitor-torch': minor
+---
+
+feat(android): add compatibility for the ML Kit Barcode Scanning plugin
--- packages/torch/android/src/main/java/io/capawesome/capacitorjs/plugins/torch/Torch.java
+++ packages/torch/android/src/main/java/io/capawesome/capacitorjs/plugins/torch/Torch.java
@@ -4,6 +4,10 @@ import android.content.pm.PackageManager;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraManager;
 import android.os.Build;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.camera.core.CameraControl;
+import java.lang.reflect.Method;
 
 public class Torch {
 
@@ -16,26 +20,12 @@ public class Torch {
     }
 
     public void enable() throws CameraAccessException {
-        CameraManager camera = (CameraManager) plugin.getContext().getSystemService(plugin.getContext().CAMERA_SERVICE);
-        String cameraId = camera.getCameraIdList()[0];
-        if (cameraId == null) {
-            return;
-        }
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            camera.setTorchMode(cameraId, true);
-        }
+        setTorchMode(true);
         isTorchEnabled = true;
     }
 
     public void disable() throws CameraAccessException {
-        CameraManager camera = (CameraManager) plugin.getContext().getSystemService(plugin.getContext().CAMERA_SERVICE);
-        String cameraId = camera.getCameraIdList()[0];
-        if (cameraId == null) {
-            return;
-        }
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            camera.setTorchMode(cameraId, false);
-        }
+        setTorchMode(false);
         isTorchEnabled = false;
     }
 
@@ -56,5 +46,45 @@ public class Torch {
         } else {
             enable();
         }
+    }
+
+    private void setTorchMode(boolean enabled) throws CameraAccessException {
+        CameraControl cameraControl = getCameraControl();
+        if (cameraControl == null) {
+            CameraManager cameraManager = getCameraManager();
+            setTorchMode(cameraManager, enabled);
+        } else {
+            setTorchMode(cameraControl, enabled);
+        }
+    }
+
+    private void setTorchMode(@NonNull CameraManager cameraManager, boolean enabled) throws CameraAccessException {
+        String cameraId = cameraManager.getCameraIdList()[0];
+        if (cameraId == null) {
+            return;
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            cameraManager.setTorchMode(cameraId, enabled);
+        }
+    }
+
+    private void setTorchMode(@NonNull CameraControl cameraControl, boolean enabled) {
+        cameraControl.enableTorch(enabled);
+    }
+
+    @Nullable
+    private CameraControl getCameraControl() {
+        try {
+            Class<?> cls = Class.forName("io.capawesome.capacitorjs.plugins.mlkit.barcodescanning.BarcodeScanner");
+            Method method = cls.getMethod("getCameraControl");
+            return (CameraControl) method.invoke(null);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    @NonNull
+    private CameraManager getCameraManager() {
+        return (CameraManager) plugin.getContext().getSystemService(plugin.getContext().CAMERA_SERVICE);
     }
 }
