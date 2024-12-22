014d9c0156023425647631b9b116a6ab512a05d4
Ehsan Barooni <66495214+ebarooni@users.noreply.github.com>
2024-12-22 14:21:18 +0100

    feat(screenshot): implement screenshot on web

--- packages/screenshot/src/web.ts
+++ packages/screenshot/src/web.ts
@@ -1,10 +1,11 @@
 import { WebPlugin } from '@capacitor/core';
+import html2canvas from 'html2canvas';
 
-import type { ScreenshotPlugin } from './definitions';
+import type { ScreenshotPlugin, TakeResult } from './definitions';
 
 export class ScreenshotWeb extends WebPlugin implements ScreenshotPlugin {
-  async echo(options: { value: string }): Promise<{ value: string }> {
-    console.log('ECHO', options);
-    return options;
+  async take(): Promise<TakeResult> {
+    const canvas = await html2canvas(document.body);
+    return { uri: canvas.toDataURL() };
   }
 }
