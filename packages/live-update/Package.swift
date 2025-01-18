366bf7707272fb95b6f5606880cc32fcb2f13821
Alex Lykesas <alexookah@me.com>
2025-01-18 16:01:56 +0200

    Use 2.4.3 version instead

--- packages/live-update/Package.swift
+++ packages/live-update/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", branch: "6.0.0"),
         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.9.0")),
-        .package(url: "https://github.com/ZipArchive/ZipArchive.git", exact: "2.2.3")
+        .package(url: "https://github.com/ZipArchive/ZipArchive.git", exact: "2.4.3")
     ],
     targets: [
         .target(
