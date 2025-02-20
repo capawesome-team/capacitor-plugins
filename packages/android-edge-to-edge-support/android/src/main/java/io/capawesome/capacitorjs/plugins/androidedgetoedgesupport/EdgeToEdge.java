b8f0fc91b45990a5f08f3c8547902d58dc473bb8
Paul Stelzer <paul.stelzer@innomobile.de>
2025-02-20 07:08:36 +0100

    fix: support different devices for edge to edge #428

--- packages/android-edge-to-edge-support/android/src/main/java/io/capawesome/capacitorjs/plugins/androidedgetoedgesupport/EdgeToEdge.java
+++ packages/android-edge-to-edge-support/android/src/main/java/io/capawesome/capacitorjs/plugins/androidedgetoedgesupport/EdgeToEdge.java
@@ -1,5 +1,6 @@
 package io.capawesome.capacitorjs.plugins.androidedgetoedgesupport;
 
+import android.os.Build;
 import android.graphics.Color;
 import android.view.View;
 import android.view.ViewGroup;
@@ -42,8 +43,21 @@ public class EdgeToEdge {
         // Apply insets to disable the edge-to-edge feature
         ViewCompat.setOnApplyWindowInsetsListener(view, (v, windowInsets) -> {
             Insets insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars());
+            Boolean keyboardVisible = windowInsets.isVisible(WindowInsetsCompat.Type.ime());
+
             ViewGroup.MarginLayoutParams mlp = (ViewGroup.MarginLayoutParams) v.getLayoutParams();
-            mlp.bottomMargin = insets.bottom;
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                mlp.bottomMargin = insets.bottom;
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) {
+                if (keyboardVisible) {
+                    mlp.bottomMargin = 0;
+                } else {
+                    mlp.bottomMargin = insets.bottom;
+                }
+            }
+
             mlp.leftMargin = insets.left;
             mlp.rightMargin = insets.right;
             mlp.topMargin = insets.top;
