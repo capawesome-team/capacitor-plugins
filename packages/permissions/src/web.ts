73c5e4007a9fcbc5b447f9d6137cacbc48833f9c
Nico Genz <mail@nicogenz.de>
2025-03-28 07:07:19 +0100

    implement interface

--- packages/permissions/src/web.ts
+++ packages/permissions/src/web.ts
@@ -1,8 +1,24 @@
 import { WebPlugin } from '@capacitor/core';
 
-import type { PermissionsPlugin } from './definitions';
+import type {
+  CheckPermissionOptions,
+  CheckPermissionResult,
+  PermissionsPlugin,
+  RequestPermissionOptions,
+  RequestPermissionResult,
+} from './definitions';
 
 export class PermissionsWeb extends WebPlugin implements PermissionsPlugin {
+  checkPermission(
+    options: CheckPermissionOptions,
+  ): Promise<CheckPermissionResult> {
+    throw new Error('Method not implemented.');
+  }
+  requestPermission(
+    options: RequestPermissionOptions,
+  ): Promise<RequestPermissionResult> {
+    throw new Error('Method not implemented.');
+  }
   async echo(options: { value: string }): Promise<{ value: string }> {
     console.log('ECHO', options);
     return options;
