44801d2135faf813f0351091684c64d440996dd3
Robin Genz <mail@robingenz.dev>
2025-05-29 13:31:46 +0200

    wip

--- packages/libsql/src/definitions.ts
+++ packages/libsql/src/definitions.ts
@@ -1,11 +1,61 @@
 export interface LibsqlPlugin {
+  /**
+   * Begin a transaction on the specified database connection.
+   *
+   * @since 0.1.0
+   */
   beginTransaction(options: BeginTransactionOptions): Promise<void>;
+  /**
+   * Commit the current transaction on the specified database connection.
+   *
+   * @since 0.1.0
+   */
   commitTransaction(options: CommitTransactionOptions): Promise<void>;
+  /**
+   * Connect to a database.
+   *
+   * This method must be called before any other methods that interact with the database.
+   *
+   * @since 0.1.0
+   */
   connect(options: ConnectOptions): Promise<ConnectResult>;
+  /**
+   * Execute a single SQL statement on the specified database connection.
+   *
+   * This method can be used to execute any SQL statement, including `INSERT`, `UPDATE`, `DELETE`, and `CREATE TABLE`.
+   *
+   * @since 0.1.0
+   */
   execute(options: ExecuteOptions): Promise<void>;
+  /**
+   * Execute a batch of SQL statements on the specified database connection.
+   *
+   * This method can be used to execute multiple SQL statements in a single call.
+   *
+   * @since 0.1.0
+   */
   executeBatch(options: ExecuteBatchOptions): Promise<void>;
+  /**
+   * Query the database and return the result set.
+   *
+   * This method can be used to execute `SELECT` statements and retrieve the result set.
+   *
+   * @since 0.1.0
+   */
   query(options: QueryOptions): Promise<QueryResult>;
+  /**
+   * Rollback the current transaction on the specified database connection.
+   *
+   * This method will undo all changes made in the current transaction.
+   *
+   * @since 0.1.0
+   */
   rollbackTransaction(options: RollbackTransactionOptions): Promise<void>;
+  /**
+   * Synchronize the database with the remote server.
+   *
+   * @since 0.1.0
+   */
   sync(options: SyncOptions): Promise<void>;
 }
 
