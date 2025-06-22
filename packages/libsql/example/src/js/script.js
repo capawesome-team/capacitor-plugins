858f1480b16371047ffb00dc86e2cb69e8808cab
Ehsan Barooni <66495214+ebarooni@users.noreply.github.com>
2025-06-22 20:28:41 +0200

    feat(libsql): add methods to example app

---
+++ packages/libsql/example/src/js/script.js
@@ -0,0 +1,74 @@
+import { Libsql } from '@capawesome/capacitor-libsql';
+
+document.addEventListener('DOMContentLoaded', () => {
+  let connectionId;
+  let transactionId;
+
+  document
+    .querySelector('#begin-transaction-button')
+    .addEventListener('click', async () => {
+      const result = await Libsql.beginTransaction({ connectionId });
+      transactionId = result.transactionId;
+      console.log('Begin transaction result:', result);
+    });
+  document
+    .querySelector('#commit-transaction-button')
+    .addEventListener('click', async () => {
+      await Libsql.commitTransaction({ transactionId });
+    });
+  document
+    .querySelector('#connect-button')
+    .addEventListener('click', async () => {
+      const result = await Libsql.connect({ path: 'mylocal.db' });
+      connectionId = result.connectionId;
+      console.log('Connect result:', result);
+    });
+  document
+    .querySelector('#execute-button')
+    .addEventListener('click', async () => {
+      await Libsql.execute({
+        connectionId,
+        statement:
+          'CREATE TABLE IF NOT EXISTS test (key TEXT PRIMARY KEY, value TEXT)',
+        transactionId,
+      });
+      await Libsql.execute({
+        connectionId,
+        statement: 'INSERT OR REPLACE INTO test (key, value) VALUES (?, ?)',
+        transactionId,
+        values: ['key1', 'value1'],
+      });
+    });
+  document
+    .querySelector('#execute-batch-button')
+    .addEventListener('click', async () => {
+      await Libsql.executeBatch({
+        connectionId,
+        statement: [
+          'CREATE TABLE IF NOT EXISTS test (key TEXT PRIMARY KEY, value TEXT)',
+          'INSERT OR REPLACE INTO test (key, value) VALUES (?, ?)'
+        ],
+        values: [[], ['key1', 'value1']],
+      });
+    });
+  document
+    .querySelector('#rollback-transaction-button')
+    .addEventListener('click', async () => {
+      await Libsql.rollbackTransaction({ connectionId, transactionId });
+    });
+  document
+    .querySelector('#query-button')
+    .addEventListener('click', async () => {
+      const result = await Libsql.query({
+        connectionId,
+        statement: 'SELECT * FROM test',
+        transactionId,
+      });
+      console.log('Query result:', result);
+    });
+  document
+    .querySelector('#sync-button')
+    .addEventListener('click', async () => {
+      await Libsql.sync({ connectionId })
+    })
+});
