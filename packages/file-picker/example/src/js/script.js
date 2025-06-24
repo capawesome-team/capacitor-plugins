ed790c6936efa59b74ff29aeb1d017bb3bcecf7a
Ehsan Barooni <66495214+ebarooni@users.noreply.github.com>
2025-06-24 12:08:40 +0200

    feat(file-picker): copyFile method on ios
    Update example app.

--- packages/file-picker/example/src/js/script.js
+++ packages/file-picker/example/src/js/script.js
@@ -1,6 +1,11 @@
 import { FilePicker } from '@capawesome/capacitor-file-picker';
 
 document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('#copy-file').addEventListener('click', async () => {
+    const from = (await FilePicker.pickFiles()).files[0].path;
+    const to = (await FilePicker.pickDirectory()).path;
+    await FilePicker.copyFile({ from, to });
+  });
   document.querySelector('#pick-files').addEventListener('click', async () => {
     await FilePicker.pickFiles();
   });
