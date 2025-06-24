667e942a51e1f4ad057fed0b8e13fab63e70e87a
Ehsan Barooni <66495214+ebarooni@users.noreply.github.com>
2025-06-24 12:07:50 +0200

    feat(file-picker): copyFile method on ios
    Add classes on ios.

---
+++ packages/file-picker/ios/Plugin/Classes/Options/CopyFileOptions.swift
@@ -0,0 +1,39 @@
+import Capacitor
+
+@objc public class CopyFileOptions: NSObject {
+    private let fromUrl: URL
+    private var overwrite = false
+    private let toUrl: URL
+
+    init(_ call: CAPPluginCall) throws {
+        guard let from = call.getString("from") else {
+            throw CustomError.fromMissing
+        }
+        guard let fromUrl = URL(string: from) else {
+            throw CustomError.invalidPath
+        }
+        self.fromUrl = fromUrl
+
+        self.overwrite = call.getBool("overwrite", self.overwrite)
+
+        guard let to = call.getString("to") else {
+            throw CustomError.toMissing
+        }
+        guard let toUrl = URL(string: to) else {
+            throw CustomError.invalidPath
+        }
+        self.toUrl = toUrl
+    }
+
+    func getFromUrl() -> URL {
+        return fromUrl
+    }
+
+    func getOverwrite() -> Bool {
+        return overwrite
+    }
+
+    func getToUrl() -> URL {
+        return toUrl
+    }
+}
