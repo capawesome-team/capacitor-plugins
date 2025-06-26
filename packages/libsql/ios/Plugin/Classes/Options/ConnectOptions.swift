94d0fa6a3cfc89c31b7d467e687c5d1e95f7ab9e
Ehsan Barooni <66495214+ebarooni@users.noreply.github.com>
2025-06-26 14:59:37 +0200

    test(libsql): use library directory for ios

--- packages/libsql/ios/Plugin/Classes/Options/ConnectOptions.swift
+++ packages/libsql/ios/Plugin/Classes/Options/ConnectOptions.swift
@@ -9,7 +9,7 @@ import Capacitor
     init(_ call: CAPPluginCall) throws {
         self.authToken = call.getString("authToken")
         if let path = call.getString("path") {
-            let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+            let documentsPath = NSSearchPathForDirectoriesInDomains(.libraryDirectory, .userDomainMask, true).first!
             let databasePath = URL(fileURLWithPath: documentsPath).appendingPathComponent(path).path
             self.path = databasePath
         } else {
