dd015e119df384af3f504f9e7a30d1100ecbfec6
Robin Genz <mail@robingenz.dev>
2025-12-14 14:13:17 +0100

    wip

--- packages/realtimekit/Package.swift
+++ packages/realtimekit/Package.swift
@@ -3,14 +3,14 @@ import PackageDescription
 
 let package = Package(
     name: "CapawesomeCapacitorRealtimekit",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "CapawesomeCapacitorRealtimekit",
             targets: ["RealtimeKitPlugin"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0"),
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0"),
         .package(url: "https://github.com/dyte-in/RealtimeKitCoreiOS.git", exact: "1.3.2"),
         .package(url: "https://github.com/dyte-in/RealtimeKitUI.git", exact: "0.4.4")
     ],
