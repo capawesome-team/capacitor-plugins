bd386571b9151d22617cccbd0eda18efc0a5df3b
Robin Genz <mail@robingenz.dev>
2025-12-16 12:30:21 +0100

    chore: update example apps to Capacitor 8

--- packages/libsql/example/ios/App/CapApp-SPM/Package.swift
+++ packages/libsql/example/ios/App/CapApp-SPM/Package.swift
@@ -4,14 +4,14 @@ import PackageDescription
 // DO NOT MODIFY THIS FILE - managed by Capacitor CLI commands
 let package = Package(
     name: "CapApp-SPM",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "CapApp-SPM",
             targets: ["CapApp-SPM"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "7.0.0"),
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.0.0"),
         .package(name: "CapawesomeCapacitorLibsql", path: "../../../..")
     ],
     targets: [
