d8a7dec57f58f1f8d17e17e963aa451f909dfa37
Robin Genz <mail@robingenz.dev>
2025-12-22 10:06:57 +0100

    fix web

--- packages/age-signals/src/web.ts
+++ packages/age-signals/src/web.ts
@@ -5,6 +5,9 @@ import type {
   CheckAgeSignalsOptions,
   CheckAgeSignalsResult,
   CheckEligibilityResult,
+  SetNextAgeSignalsExceptionOptions,
+  SetNextAgeSignalsResultOptions,
+  SetUseFakeManagerOptions,
 } from './definitions';
 
 export class AgeSignalsWeb extends WebPlugin implements AgeSignalsPlugin {
@@ -15,6 +18,24 @@ export class AgeSignalsWeb extends WebPlugin implements AgeSignalsPlugin {
   }
 
   async checkEligibility(): Promise<CheckEligibilityResult> {
+    throw this.unimplemented('Not implemented on web.');
+  }
+
+  async setUseFakeManager(
+    _options: SetUseFakeManagerOptions,
+  ): Promise<void> {
+    throw this.unimplemented('Not implemented on web.');
+  }
+
+  async setNextAgeSignalsResult(
+    _options: SetNextAgeSignalsResultOptions,
+  ): Promise<void> {
+    throw this.unimplemented('Not implemented on web.');
+  }
+
+  async setNextAgeSignalsException(
+    _options: SetNextAgeSignalsExceptionOptions,
+  ): Promise<void> {
     throw this.unimplemented('Not implemented on web.');
   }
 }
