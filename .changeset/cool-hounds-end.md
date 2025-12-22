27ee06a17c765f4566da9142dcb51b7d198a857e
Robin Genz <mail@robingenz.dev>
2025-12-22 09:30:44 +0100

    feat(live-update): add support for iOS 15 (#721)
    * refactor(live-update): replace `ZipArchive` with `ZIPFoundation`
    
    * bump version

---
+++ .changeset/cool-hounds-end.md
@@ -0,0 +1,5 @@
+---
+'@capawesome/capacitor-live-update': minor
+---
+
+feat(ios): add support for iOS 15
--- packages/live-update/Package.swift
+++ packages/live-update/Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "CapawesomeCapacitorLiveUpdate",
-    platforms: [.iOS(.v16)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "CapawesomeCapacitorLiveUpdate",
@@ -12,7 +12,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0"),
         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMinor(from: "5.10.2")),
-        .package(url: "https://github.com/ZipArchive/ZipArchive.git", .upToNextMinor(from: "2.6.0"))
+        .package(url: "https://github.com/weichsel/ZIPFoundation.git", .upToNextMinor(from: "0.9.0"))
     ],
     targets: [
         .target(
@@ -21,7 +21,7 @@ let package = Package(
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
                 .product(name: "Cordova", package: "capacitor-swift-pm"),
                 .product(name: "Alamofire", package: "Alamofire"),
-                .product(name: "ZipArchive", package: "ZipArchive")
+                .product(name: "ZIPFoundation", package: "ZIPFoundation")
             ],
             path: "ios/Plugin"),
         .testTarget(
