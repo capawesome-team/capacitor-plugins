976f4ce96ea5a4f84fa1fdc3eb0c9815c9e5dbc3
anfuerer <admail@gmx.ch>
2026-02-20 16:28:36 +0100

    fix(background-task): ensure thread-safety of the taskIds dictionary

--- packages/background-task/ios/Plugin/BackgroundTask.swift
+++ packages/background-task/ios/Plugin/BackgroundTask.swift
@@ -2,23 +2,30 @@ import Foundation
 import UIKit
 
 @objc public class BackgroundTask: NSObject {
-    var taskIds: [String: UIBackgroundTaskIdentifier] = [:]
+    private let isolationQueue = DispatchQueue(label: "io.capawesome.backgroundtask.isolation")
+    private var taskIds: [String: UIBackgroundTaskIdentifier] = [:]
 
     @objc public func beforeExit(_ callbackId: String) {
-        DispatchQueue.global().async {
+        // Use the isolation queue to synchronize access to the taskIds dictionary
+        isolationQueue.async { [weak self] in
+            guard let self = self else { return }
+            
             var taskId = UIBackgroundTaskIdentifier.invalid
             taskId = UIApplication.shared.beginBackgroundTask {
-                // Finish the task if time expires.
-                UIApplication.shared.endBackgroundTask(taskId)
-                self.taskIds.removeValue(forKey: callbackId)
+                // Finish the task if time expires. This expiration handler can run on any thread,
+                // so we MUST hop back to the isolation queue.
+                self.isolationQueue.async {
+                    UIApplication.shared.endBackgroundTask(taskId)
+                    self.taskIds.removeValue(forKey: callbackId)
+                }
             }
             self.taskIds[callbackId] = taskId
         }
     }
 
     @objc public func finish(_ callbackId: String) {
-        DispatchQueue.global().async {
-            guard let taskId = self.taskIds[callbackId] else {
+        isolationQueue.async { [weak self] in
+            guard let self = self, let taskId = self.taskIds[callbackId] else {
                 return
             }
             UIApplication.shared.endBackgroundTask(taskId)
