5cc24a543faa5a3522eb0796f04b7ec60076f0dc
Robin Genz <mail@robingenz.dev>
2026-09-21 13:41:43 +0200

    chore: exclude all `example*` directories from swiftlint (#1073)

--- swiftlint.config.js
+++ swiftlint.config.js
@@ -3,8 +3,8 @@ const base = require('@ionic/swiftlint-config');
 module.exports = {
   ...base,
   excluded: [
-    ...(base.excluded || []).map((p) => p.replace('${PWD}', process.cwd())),
+    ...(base.excluded || []).map(p => p.replace('${PWD}', process.cwd())),
     `${process.cwd()}/.build`,
-    `${process.cwd()}/example`,
+    `${process.cwd()}/example*`,
   ],
 };
